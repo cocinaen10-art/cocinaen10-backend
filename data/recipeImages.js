2f1d538ad712a4251397a8fe1995928400335b77
Maria <cocinaen10@gmail.com>
2026-06-04 23:17:24 +0200

    remove wrong chicken image

--- data/recipeImages.js
+++ data/recipeImages.js
@@ -2,7 +2,6 @@ const recipeImages = {
   pollo: [
     "https://images.unsplash.com/photo-1604908176997-125f25cc6f3d",
     "https://images.unsplash.com/photo-1527477396000-e27163b481c2",
-    "https://images.unsplash.com/photo-1600891964092-4316c288032e",
   ],
 
   pasta: [
