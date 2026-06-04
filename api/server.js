8332480fdcd930cd05408da46c8282e7e1645125
Maria <cocinaen10@gmail.com>
2026-06-04 17:21:55 +0200

    fix require path

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const recipeImages = require("./data/recipeImages");
+const recipeImages = require("../data/recipeImages");
 
 app.use(cors());
 app.use(express.json());
