f828f70ad58c335187641c5995650bc5ef2664ba
Maria <cocinaen10@gmail.com>
2026-06-08 16:08:04 +0200

    improve recipe prompt

--- data/recipeImages.js
+++ data/recipeImages.js
@@ -1,7 +1,9 @@
 const recipeImages = {
   pollo: [
-  "https://images.pexels.com/photos/616354/pexels-photo-616354.jpeg",
-],
+    "https://images.pexels.com/photos/616354/pexels-photo-616354.jpeg",
+    "https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg",
+    "https://images.pexels.com/photos/2338407/pexels-photo-2338407.jpeg",
+  ],
 
   pasta: [
     "url1",
